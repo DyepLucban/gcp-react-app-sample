ca3369eddbdc8810fc0448337b271c950842c1ae
Globoy <lucbanjep@gmail.com>
2021-09-06 14:18:50 +0800

    update changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          This is a test push changes.
+          This is a test push changes via cloud build.
         </p>
         <a
           className="App-link"
