74c6b5a3c8af510d92f898ffc291c7512bf16260
Globoy <lucbanjep@gmail.com>
2021-09-22 16:40:19 +0800

    Added magic bell

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import MagicBell, { FloatingNotificationInbox } from '@magicbell/magicbell-react';
 
 function App() {
+  let MAGICBELL_API_KEY = '2709b1fc121ce26869d683a4bc56aa5fdd233630'
   return (
     <div className="App">
       <header className="App-header">
@@ -13,6 +15,9 @@ function App() {
         <p>
           This changes is came from my new branch.
         </p>
+        <MagicBell apiKey={MAGICBELL_API_KEY} userEmail="lucbanjep@gmail.com">
+          {(props) => <FloatingNotificationInbox height={300} {...props} />}
+        </MagicBell>        
         <a
           className="App-link"
           href="https://reactjs.org"
